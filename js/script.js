bac4024b02212fad95a7521ed087d623ae64c82f
Lonyon486 <yefrey.gutierrez@uptc.edu.co>
2021-11-24 16:41:11 -0500

    Ruta archivos especificos del proyecto:
    index.html
    css/style.css
    js/script.js

---
+++ js/script.js
@@ -0,0 +1,78 @@
+console.log("script loaded");
+var form1 = document.getElementById("form-1");
+
+form1.addEventListener("submit", function(event){
+	event.preventDefault();
+	console.log(event);
+	console.log("submit");
+});
+
+//capture name
+
+form1.addEventListener("submit", function(event){
+
+	/*
+	var name = form1.elements["name"].value;
+	var lastName = form1.elements["lastName"].value;
+	var phoneNumber = form1.elements["phoneNumber"].value;
+	var address = form1.elements["address"].value;
+	var size = form1.elements["size"].value;
+	var email = form1.elements["email"].value;
+	var password = form1.elements["password"].value;
+	var verifyEmail = form1.elements["verifyEmail"].value;
+	var verifyPassword = form1.elements["verifyPassword"].value;
+	var bio = form1.elements["bio"].value;
+	let birthDate = new Date();
+	birthDate = form1.elements["birthDate"].value;
+
+	event.preventDefault();
+	console.log(event);
+	console.log(name 
+		+ " " + lastName  
+		+ " " + phoneNumber  
+		+ " " + address  
+		+ " " + size 
+		+ " " + birthDate 
+		+ " " + email  
+		+ " " + password 
+		+ " " + verifyEmail 
+		+ " " + verifyPassword 
+		+ " " + bio);
+
+	document.write("Los datos que ingresó son:" + name 
+		+ " " + lastName  
+		+ " " + phoneNumber  
+		+ " " + address  
+		+ " " + size 
+		+ " " + birthDate 
+		+ " " + email  
+		+ " " + password 
+		+ " " + verifyEmail 
+		+ " " + verifyPassword 
+		+ " " + bio);
+	document.getElementById("data_result").value = (name 
+		+ " " + lastName  
+		+ " " + phoneNumber  
+		+ " " + address  
+		+ " " + size 
+		+ " " + birthDate 
+		+ " " + email  
+		+ " " + password 
+		+ " " + verifyEmail 
+		+ " " + verifyPassword 
+		+ " " + bio);
+		*/
+		var data = Object.fromEntries(new FormData(event.target).entries());
+		var result = document.getElementById("data_result");
+		result.innerHTML = data.name
+			+ " " + data.lastName
+			+ " " + data.phoneNumber
+			+ " " + data.address
+			+ " " + data.size
+			+ " " + data.birthDate
+			+ " " + data.email
+			+ " " + data.password
+			+ " " + data.verifyEmail
+			+ " " + data.verifyPassword
+			+ " " + data.bio;
+});
