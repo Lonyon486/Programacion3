8f22beb51f6b404a9af9cf8f56edff49cdc8f653
Lonyon486 <yefrey.gutierrez@uptc.edu.co>
2021-12-16 15:20:27 -0500

    localstorage implementation
    implementation of tasks to the main branch

--- js/script.js
+++ js/script.js
@@ -5,6 +5,46 @@ form1.addEventListener("submit", function(event){
 	event.preventDefault();
 	console.log(event);
 	console.log("submit");
+
+});
+
+
+var img1 = document.getElementById("image1");
+var img2 = document.getElementById("image2");
+var img3 = document.getElementById("image3");
+var img4 = document.getElementById("image4");
+var img5 = document.getElementById("image5");
+var img6 = document.getElementById("image6");
+var img7 = document.getElementById("image7");
+var img8 = document.getElementById("image8");
+
+//firstImage.onclick = firstImage.classList.add("card-img-topEdit");
+
+//function that modify the class of the image
+
+img1.addEventListener("click",function(){
+	img1.classList.add("card-img-topEdit");
+});
+img2.addEventListener("click",function(){
+	img2.classList.add("card-img-topEdit");
+});
+img3.addEventListener("click",function(){
+	img3.classList.add("card-img-topEdit");
+});
+img4.addEventListener("click",function(){
+	img4.classList.add("card-img-topEdit");
+});
+img5.addEventListener("click",function(){
+	img5.classList.add("card-img-topEdit");
+});
+img6.addEventListener("click",function(){
+	img6.classList.add("card-img-topEdit");
+});
+img7.addEventListener("click",function(){
+	img7.classList.add("card-img-topEdit");
+});
+img8.addEventListener("click",function(){
+	img8.classList.add("card-img-topEdit");
 });
 
 //capture name
@@ -61,9 +101,25 @@ form1.addEventListener("submit", function(event){
 		+ " " + verifyEmail 
 		+ " " + verifyPassword 
 		+ " " + bio);
+
 		*/
+
+		/*
 		var data = Object.fromEntries(new FormData(event.target).entries());
 		var result = document.getElementById("data_result");
+		var container = {
+			data.name, data.lastName, data.birthDate
+		};
+		console.log(container);
+
+		result.innerHTML = data.name 
+			+ " " + data.lastName
+			+ " " + data.birthDate;
+
+		*/
+
+		/*
+
 		result.innerHTML = data.name
 			+ " " + data.lastName
 			+ " " + data.phoneNumber
@@ -75,4 +131,35 @@ form1.addEventListener("submit", function(event){
 			+ " " + data.verifyEmail
 			+ " " + data.verifyPassword
 			+ " " + data.bio;
+		*/
+
+});
+
+var count = 1;
+
+form1.addEventListener("submit", function(event){
+	var container = [];
+	var data = Object.fromEntries(new FormData(event.target).entries());
+	var result = document.getElementById("data_result");
+	var result_localStorage = document.getElementById("data_storage");
+	container.push(data.name, data.lastName, data.birthDate);
+
+	localStorage.setItem(count, container);
+	count++;
+	var aux = "";
+
+	for(i = 0; i < localStorage.length; i++){
+
+		var data = localStorage.getItem(localStorage.key(i));
+		aux += "Value: " + data + "\n"; 
+	}
+	console.log(aux);
+	
+	//var container = [data.name, data.lastName, data.birthDate];
+	console.log(container);
+	result_localStorage.innerHTML = aux;
+	result.innerHTML = data.name 
+			+ " " + data.lastName
+			+ " " + data.birthDate;
+	
 });
